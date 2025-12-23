d90d5b8d9f70ffa52e2540c0f71e1c5d83594a29
tusharpatel0504 <tushar789patel@gmail.com>
2025-12-23 15:14:05 +0530

    components/course/details

--- app/components/course/details/page.tsx
+++ app/components/course/details/page.tsx
@@ -1,8 +1,10 @@
 import CourseDetailsHero from "../../CourseDetailsHero";
+import Navbar from "../../Navbar";
 
 export default function Page(){
     return (
         <>
+        <Navbar/>
             <CourseDetailsHero/>
         </>
     )
